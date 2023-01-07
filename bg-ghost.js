0ff7cf01363a3d9b7d31f673e655b7b07430d54b
Shrihari Mohan <shrihari.uma.1999@gmail.com>
2023-01-07 15:58:25 +0530

    Add - Find Ghost CSS

---
+++ bg-ghost.js
@@ -0,0 +1,42 @@
+const addGhostCss = async (tabId) => {
+  await chrome.scripting.insertCSS({
+    files: ["ghost.css"],
+    target: { tabId },
+  });
+
+  chrome.action.setBadgeBackgroundColor({
+    color: "#DA0037"
+  });
+}
+
+const removeGhostCss = async (tabId) => {
+  await chrome.scripting.removeCSS({
+    files: ["ghost.css"],
+    target: { tabId },
+  });
+
+  chrome.action.setBadgeBackgroundColor({
+    color: "#171717"
+  });
+}
+chrome.action.onClicked.addListener(async (tab) => {
+  const prevState = await chrome.action.getBadgeText({ tabId: tab.id });
+  const nextState = prevState === 'ON' ? 'OFF' : 'ON'
+  if ( nextState == 'ON') {
+    await addGhostCss(tab.id)
+  }
+  else {
+    await removeGhostCss(tab.id)
+  }
+  chrome.action.setBadgeText({text: nextState , tabId: tab.id });
+});
+
+chrome.runtime.onInstalled.addListener(() => {
+  chrome.action.setBadgeText({
+    text: "OFF"
+  });
+
+  chrome.action.setBadgeBackgroundColor({
+    color: "#171717"
+  });
+});
